bfaa502555dbf9d194f94e6c4572aafae94eaec2
landyeath <landyeath@gmail.com>
2016-11-14 14:50:04 +0800

    use standard API

--- common/helper.py
+++ common/helper.py
@@ -10,51 +10,52 @@
 import json
 import math
 
+COIN_TYPE_BTC_CNY = "btc_cny"
+COIN_TYPE_LTC_CNY = "ltc_cny"
+HUOBI_COIN_TYPE_BTC = 1
+HUOBI_COIN_TYPE_LTC = 2
+
 coinTypeStructure = {
-    "btc_cny" : {
+    COIN_TYPE_BTC_CNY : {
         "huobi" : {
-            "coin_type" : 1,
+            "coin_type" : HUOBI_COIN_TYPE_BTC,
             "market"    : "cny",
-            "coin_str"  : "huobi_btc",
-            "market_str" : "huobi_cny",
-            "pricer_str" : "huobi-cnybtc"
+            "coin_str"  : "huobi_cny_btc",
+            "market_str" : "huobi_cny_cash"
         },
         "okcoin" : {
-            "coin_type" : "btc_cny",
+            "coin_type" : COIN_TYPE_BTC_CNY,
             "market"    : "cny",
-            "coin_str"  : "okcoin_btc",
-            "market_str" : "okcoin_cny",
+            "coin_str"  : "okcoin_cny_btc",
+            "market_str" : "okcoin_cny_cash"
         }
     },
-    "ltc_cny" : {
+    COIN_TYPE_LTC_CNY : {
         "huobi" : {
-            "coin_type" : 2,
+            "coin_type" : HUOBI_COIN_TYPE_LTC,
             "market"    : "cny",
-            "coin_str"  : "huobi_ltc",
-            "market_str" : "huobi_cny",
-            "pricer_str" : "huobi-cnyltc"
+            "coin_str"  : "huobi_cny_ltc",
+            "market_str" : "huobi_cny_cash"
         },
         "okcoin" : {
-            "coin_type" : "ltc_cny",
+            "coin_type" : COIN_TYPE_LTC_CNY,
             "market"    : "cny",
-            "coin_str"  : "okcoin_ltc",
-            "market_str" : "okcoin_cny",
+            "coin_str"  : "okcoin_cny_ltc",
+            "market_str" : "okcoin_cny_cash"
         }
     }
 }
 
-COIN_TYPE_BTC_CNY = "btc_cny"
-COIN_TYPE_LTC_CNY = "ltc_cny"
 
 def getDictFromJSONString(str):
     return json.loads(str)
 
 def getRoundedQuantity(qty, coin_type):
     if coin_type == COIN_TYPE_BTC_CNY:
-        #按照okcoin的下单规则，比特币都是0.01btc的整数倍，取下限
+        #按照okcoin的下单规则，比特币都是0.01 btc的整数倍，取下限
         return int(qty*100)/100
     elif coin_type == COIN_TYPE_LTC_CNY:
-        #按照okcoin的下单规则，莱特币都是0.1ltc的整数倍，取下限
+        #按照okcoin的下单规则，莱特币都是0.1 ltc的整数倍，取下限
         return int(qty*10)/10
     else:
         raise ValueError( "invalid coin type %s" % coin_type)
@@ -62,3 +63,4 @@ def getRoundedQuantity(qty, coin_type):
 
 def myRound(qty, decimal_places=4):
     return int(qty * math.pow(10, decimal_places))/int(math.pow(10,decimal_places))
+
--- liveStrategyEngine/BanZhuan.py
+++ liveStrategyEngine/BanZhuan.py
@@ -24,19 +24,16 @@ class BanzhuanStrategy(object):
         self.orderWaitingTime = orderWaitingTime  # 每次等待订单执行的最长时间
         self.dataLogFixedTimeWindow = dataLogFixedTimeWindow  # in seconds
         self.coinMarketType = coinMarketType
-        '''
-        helper.COIN_TYPE_BTC_CNY = "btc_cny"
-        helper.COIN_TYPE_LTC_CNY = "ltc_cny"
-        '''
-
         self.dailyExitTime = dailyExitTime
-
         self.TimeFormatForFileName = "%Y%m%d%H%M%S%f"
         self.TimeFormatForLog = "%Y-%m-%d %H:%M:%S.%f"
         self.OKCoinService = getOkcoinSpot()
         self.HuobiService = HuobiService
-        self.huobi_min = self.HuobiService.getMinimumOrderQty(helper.coinTypeStructure[self.coinMarketType]["huobi"]["coin_type"])
-        self.okcoin_min = self.OKCoinService.getMinimumOrderQty(helper.coinTypeStructure[self.coinMarketType]["okcoin"]["coin_type"])
+        self.huobi_min_quantity = self.HuobiService.getMinimumOrderQty(helper.coinTypeStructure[self.coinMarketType]["huobi"]["coin_type"])
+        self.huobi_min_cash_amount = self.HuobiService.getMinimumOrderCashAmount()
+        self.okcoin_min_quantity = self.OKCoinService.getMinimumOrderQty(helper.coinTypeStructure[self.coinMarketType]["okcoin"]["coin_type"])
+        #okcoin 的比特币最小市价买单下单金额是：0.01BTC*比特币当时市价
+        #okcoin 的莱特币最小市价买单下单金额是：0.1LTC*莱特币当时市价
         self.last_data_log_time = None
 
         # setup timeLogger
@@ -77,49 +74,49 @@ class BanzhuanStrategy(object):
 
     def getAccuntInfo(self):
         huobiAcct = self.HuobiService.getAccountInfo(helper.coinTypeStructure[self.coinMarketType]["huobi"]["market"],ACCOUNT_INFO)
-        huobi_cny = float(huobiAcct[u'available_cny_display'])
-        huobi_btc = float(huobiAcct[u'available_btc_display'])
-        huobi_ltc = float(huobiAcct[u'available_ltc_display'])
-        huobi_cny_loan = float(huobiAcct[u'loan_cny_display'])
-        huobi_btc_loan = float(huobiAcct[u'loan_btc_display'])
-        huobi_ltc_loan = float(huobiAcct[u'loan_ltc_display'])
-        huobi_cny_frozen = float(huobiAcct[u'frozen_cny_display'])
-        huobi_btc_frozen = float(huobiAcct[u'frozen_btc_display'])
-        huobi_ltc_frozen = float(huobiAcct[u'frozen_ltc_display'])
-        huobi_total = float(huobiAcct[u'total'])
-        huobi_net = float(huobiAcct[u'net_asset'])
+        huobi_cny_cash = float(huobiAcct[u'available_cny_display'])
+        huobi_cny_btc = float(huobiAcct[u'available_btc_display'])
+        huobi_cny_ltc = float(huobiAcct[u'available_ltc_display'])
+        huobi_cny_cash_loan = float(huobiAcct[u'loan_cny_display'])
+        huobi_cny_btc_loan = float(huobiAcct[u'loan_btc_display'])
+        huobi_cny_ltc_loan = float(huobiAcct[u'loan_ltc_display'])
+        huobi_cny_cash_frozen = float(huobiAcct[u'frozen_cny_display'])
+        huobi_cny_btc_frozen = float(huobiAcct[u'frozen_btc_display'])
+        huobi_cny_ltc_frozen = float(huobiAcct[u'frozen_ltc_display'])
+        huobi_cny_total = float(huobiAcct[u'total'])
+        huobi_cny_net = float(huobiAcct[u'net_asset'])
 
         okcoinAcct = self.OKCoinService.userinfo()
-        okcoin_cny = float(okcoinAcct["info"]["funds"]["free"]["cny"])
-        okcoin_btc = float(okcoinAcct["info"]["funds"]["free"]["btc"])
-        okcoin_ltc = float(okcoinAcct["info"]["funds"]["free"]["ltc"])
-        okcoin_cny_frozen = float(okcoinAcct["info"]["funds"]["freezed"]["cny"])
-        okcoin_btc_frozen = float(okcoinAcct["info"]["funds"]["freezed"]["btc"])
-        okcoin_ltc_frozen = float(okcoinAcct["info"]["funds"]["freezed"]["ltc"])
-        okcoin_total = float(okcoinAcct["info"]["funds"]["asset"]["total"])
-        okcoin_net = float(okcoinAcct["info"]["funds"]["asset"]["net"])
-        total_net = huobi_net + okcoin_net
+        okcoin_cny_cash = float(okcoinAcct["info"]["funds"]["free"]["cny"])
+        okcoin_cny_btc = float(okcoinAcct["info"]["funds"]["free"]["btc"])
+        okcoin_cny_ltc = float(okcoinAcct["info"]["funds"]["free"]["ltc"])
+        okcoin_cny_cash_frozen = float(okcoinAcct["info"]["funds"]["freezed"]["cny"])
+        okcoin_cny_btc_frozen = float(okcoinAcct["info"]["funds"]["freezed"]["btc"])
+        okcoin_cny_ltc_frozen = float(okcoinAcct["info"]["funds"]["freezed"]["ltc"])
+        okcoin_cny_total = float(okcoinAcct["info"]["funds"]["asset"]["total"])
+        okcoin_cny_net = float(okcoinAcct["info"]["funds"]["asset"]["net"])
+        total_net = huobi_cny_net + okcoin_cny_net
         return {
-            "huobi_cny": huobi_cny,
-            "huobi_btc": huobi_btc,
-            "huobi_ltc": huobi_ltc,
-            "huobi_cny_loan": huobi_cny_loan,
-            "huobi_btc_loan": huobi_btc_loan,
-            "huobi_ltc_loan": huobi_ltc_loan,
-            "huobi_cny_frozen": huobi_cny_frozen,
-            "huobi_btc_frozen": huobi_btc_frozen,
-            "huobi_ltc_frozen": huobi_ltc_frozen,
-            "huobi_total": huobi_total,
-            "huobi_net": huobi_net,
+            "huobi_cny_cash": huobi_cny_cash,
+            "huobi_cny_btc": huobi_cny_btc,
+            "huobi_cny_ltc": huobi_cny_ltc,
+            "huobi_cny_cash_loan": huobi_cny_cash_loan,
+            "huobi_cny_btc_loan": huobi_cny_btc_loan,
+            "huobi_cny_ltc_loan": huobi_cny_ltc_loan,
+            "huobi_cny_cash_frozen": huobi_cny_cash_frozen,
+            "huobi_cny_btc_frozen": huobi_cny_btc_frozen,
+            "huobi_cny_ltc_frozen": huobi_cny_ltc_frozen,
+            "huobi_cny_total": huobi_cny_total,
+            "huobi_cny_net": huobi_cny_net,
 
-            "okcoin_cny": okcoin_cny,
-            "okcoin_btc": okcoin_btc,
-            "okcoin_ltc": okcoin_ltc,
-            "okcoin_cny_frozen": okcoin_cny_frozen,
-            "okcoin_btc_frozen": okcoin_btc_frozen,
-            "okcoin_ltc_frozen": okcoin_ltc_frozen,
-            "okcoin_total": okcoin_total,
-            "okcoin_net": okcoin_net,
+            "okcoin_cny_cash": okcoin_cny_cash,
+            "okcoin_cny_btc": okcoin_cny_btc,
+            "okcoin_cny_ltc": okcoin_cny_ltc,
+            "okcoin_cny_cash_frozen": okcoin_cny_cash_frozen,
+            "okcoin_cny_btc_frozen": okcoin_cny_btc_frozen,
+            "okcoin_cny_ltc_frozen": okcoin_cny_ltc_frozen,
+            "okcoin_cny_total": okcoin_cny_total,
+            "okcoin_cny_net": okcoin_cny_net,
 
             "total_net": total_net,
         }
@@ -130,25 +127,25 @@ class BanzhuanStrategy(object):
             t = datetime.datetime.now()
             content = "%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s|%s" % \
                       (t.strftime(self.TimeFormatForLog),
-                       accountInfo["huobi_cny"],
-                       accountInfo["huobi_btc"],
-                       accountInfo["huobi_ltc"],
-                       accountInfo["huobi_cny_loan"],
-                       accountInfo["huobi_btc_loan"],
-                       accountInfo["huobi_ltc_loan"],
-                       accountInfo["huobi_cny_frozen"],
-                       accountInfo["huobi_btc_frozen"],
-                       accountInfo["huobi_ltc_frozen"],
-                       accountInfo["huobi_total"],
-                       accountInfo["huobi_net"],
-                       accountInfo["okcoin_cny"],
-                       accountInfo["okcoin_btc"],
-                       accountInfo["okcoin_ltc"],
-                       accountInfo["okcoin_cny_frozen"],
-                       accountInfo["okcoin_btc_frozen"],
-                       accountInfo["okcoin_ltc_frozen"],
-                       accountInfo["okcoin_total"],
-                       accountInfo["okcoin_net"],
+                       accountInfo["huobi_cny_cash"],
+                       accountInfo["huobi_cny_btc"],
+                       accountInfo["huobi_cny_ltc"],
+                       accountInfo["huobi_cny_cash_loan"],
+                       accountInfo["huobi_cny_btc_loan"],
+                       accountInfo["huobi_cny_ltc_loan"],
+                       accountInfo["huobi_cny_cash_frozen"],
+                       accountInfo["huobi_cny_btc_frozen"],
+                       accountInfo["huobi_cny_ltc_frozen"],
+                       accountInfo["huobi_cny_total"],
+                       accountInfo["huobi_cny_net"],
+                       accountInfo["okcoin_cny_cash"],
+                       accountInfo["okcoin_cny_btc"],
+                       accountInfo["okcoin_cny_ltc"],
+                       accountInfo["okcoin_cny_cash_frozen"],
+                       accountInfo["okcoin_cny_btc_frozen"],
+                       accountInfo["okcoin_cny_ltc_frozen"],
+                       accountInfo["okcoin_cny_total"],
+                       accountInfo["okcoin_cny_net"],
                        accountInfo["total_net"])
             self.last_data_log_time = t
         self.dataLogger.info("%s" % str(content))
@@ -162,14 +159,12 @@ class BanzhuanStrategy(object):
             tmp = helper.myRound(tmp,4)
             quantity = str(tmp)
             self.timeLog("做完小数点处理后的下单数量:%s"%quantity)
-            if float(quantity)<self.huobi_min:
+            if float(quantity)<self.huobi_min_quantity:
                 self.timeLog(
-                            "数量:%s 小于交易所最小交易数量(火币最小数量:%f),因此无法下单,此处忽略该信号"%(quantity, self.huobi_min))
+                            "数量:%s 小于交易所最小交易数量(火币最小数量:%f),因此无法下单,此处忽略该信号"%(quantity, self.huobi_min_quantity))
                 return None
-            if security == helper.COIN_TYPE_BTC_CNY:
-                coin_type = 1
-            else:
-                coin_type = 2
+
+            coin_type = helper.coinTypeStructure[self.coinMarketType]["huobi"]["coin_type"]
             res = self.HuobiService.sellMarket(coin_type, quantity, None, None, helper.coinTypeStructure[self.coinMarketType]["huobi"]["market"],SELL_MARKET)
             if u"result" not in res or res[u"result"] != u'success':
                 self.timeLog("下达火币市价卖单（数量：%s）失败！"%quantity)
@@ -196,9 +191,9 @@ class BanzhuanStrategy(object):
             tmp = helper.myRound(tmp,2)
             quantity = str(tmp)
             self.timeLog("做完小数点处理后的下单数量:%s"%quantity)
-            if float(quantity)<self.okcoin_min:
+            if float(quantity)<self.okcoin_min_quantity:
                 self.timeLog(
-                            "数量:%s 小于交易所最小交易数量(火币最小数量:%f),因此无法下单,此处忽略该信号"%(quantity, self.okcoin_min))
+                            "数量:%s 小于交易所最小交易数量(火币最小数量:%f),因此无法下单,此处忽略该信号"%(quantity, self.okcoin_min_quantity))
                 return None
             res = self.OKCoinService.trade(helper.coinTypeStructure[self.coinMarketType]["okcoin"]["coin_type"], "sell_market", amount=quantity)
             if "result" not in res or res["result"] != True:
@@ -229,14 +224,11 @@ class BanzhuanStrategy(object):
             cash_amount = str(tmp)
             self.timeLog("做完小数点处理后的下单金额:%s"%cash_amount)
 
-            if float(cash_amount)<1:
-                self.timeLog("金额:%s 小于交易所最小交易金额(火币最小金额:1元),因此无法下单,此处忽略该信号"%cash_amount)
+            if float(cash_amount)<self.huobi_min_cash_amount:
+                self.timeLog("金额:%s 小于交易所最小交易金额(火币最小金额:1元),因此无法下单,此处忽略该信号"%(cash_amount,self.huobi_min_cash_amount))
                 return None
 
-            if security == helper.COIN_TYPE_BTC_CNY:
-                coin_type = 1
-            else:
-                coin_type = 2
+            coin_type = helper.coinTypeStructure[self.coinMarketType]["huobi"]["coin_type"]
             res = self.HuobiService.buyMarket(coin_type, cash_amount, None, None, helper.coinTypeStructure[self.coinMarketType]["huobi"]["market"],BUY_MARKET)
             if u"result" not in res or res[u"result"] != u'success':
                 self.timeLog("下达火币市价买单（金额：%s）失败！"%cash_amount)
@@ -266,9 +258,9 @@ class BanzhuanStrategy(object):
             cash_amount = str(tmp)
             self.timeLog("做完小数点处理后的下单金额:%s"%cash_amount)
 
-            if float(cash_amount)<self.okcoin_min*sell_1_price:
+            if float(cash_amount)<self.okcoin_min_quantity*sell_1_price:
                 self.timeLog(
-                            "金额:%s 不足以购买交易所最小交易数量(okcoin最小数量:%f，当前卖一价格%.2f,最小金额要求：%.2f),因此无法下单,此处忽略该信号"%(cash_amount,self.okcoin_min,sell_1_price,self.okcoin_min*sell_1_price))
+                            "金额:%s 不足以购买交易所最小交易数量(okcoin最小数量:%f，当前卖一价格%.2f,最小金额要求：%.2f),因此无法下单,此处忽略该信号"%(cash_amount,self.okcoin_min_quantity,sell_1_price,self.okcoin_min_quantity*sell_1_price))
                 return None
             res = self.OKCoinService.trade(helper.coinTypeStructure[self.coinMarketType]["okcoin"]["coin_type"], "buy_market",price=cash_amount)
 
@@ -293,7 +285,7 @@ class BanzhuanStrategy(object):
     def go(self):
         self.timeLog("日志启动于 %s" % self.getStartRunningTime().strftime(self.TimeFormatForLog))
         self.dataLog(
-            content="time|huobi_cny|huobi_btc|huobi_ltc|huobi_cny_loan|huobi_btc_loan|huobi_ltc_loan|huobi_cny_frozen|huobi_btc_frozen|huobi_ltc_frozen|huobi_total|huobi_net|okcoin_cny|okcoin_btc|okcoin_ltc|okcoin_cny_frozen|okcoin_btc_frozen|okcoin_ltc_frozen|okcoin_total|okcoin_net|total_net")
+            content="time|huobi_cny_cash|huobi_cny_btc|huobi_cny_ltc|huobi_cny_cash_loan|huobi_cny_btc_loan|huobi_cny_ltc_loan|huobi_cny_cash_frozen|huobi_cny_btc_frozen|huobi_cny_ltc_frozen|huobi_cny_total|huobi_cny_net|okcoin_cny_cash|okcoin_cny_btc|okcoin_cny_ltc|okcoin_cny_cash_frozen|okcoin_cny_btc_frozen|okcoin_cny_ltc_frozen|okcoin_cny_total|okcoin_cny_net|total_net")
         self.dataLog()
 
         while (True):
@@ -342,18 +334,18 @@ class BanzhuanStrategy(object):
                 Qty = helper.myRound(Qty,4)
                 Qty = helper.getRoundedQuantity(Qty, self.coinMarketType)
 
-                if Qty < self.huobi_min or Qty < self.okcoin_min:
+                if Qty < self.huobi_min_quantity or Qty < self.okcoin_min_quantity:
                     self.timeLog(
-                        "数量:%f 小于交易所最小交易数量(火币最小数量:%f, okcoin最小数量:%f),因此无法下单并忽略该信号" %(Qty, self.huobi_min, self.okcoin_min))
+                        "数量:%f 小于交易所最小交易数量(火币最小数量:%f, okcoin最小数量:%f),因此无法下单并忽略该信号" %(Qty, self.huobi_min_quantity, self.okcoin_min_quantity))
                     continue
                 else:
                     # step1: 先处理卖
                     executed_qty = self.sell(self.coinMarketType,str(Qty),exchange="huobi")
-
-                    # step2: 再执行买
-                    Qty2 = min(executed_qty, Qty)
-                    Qty2 = max(helper.getRoundedQuantity(Qty2,self.coinMarketType),self.okcoin_min)
-                    self.buy(self.coinMarketType,str(Qty2*okcoin_sell_1_price),exchange="okcoin",sell_1_price=okcoin_sell_1_price)
+                    if executed_qty is not None:
+                        # step2: 再执行买
+                        Qty2 = min(executed_qty, Qty)
+                        Qty2 = max(helper.getRoundedQuantity(Qty2,self.coinMarketType),self.okcoin_min_quantity)
+                        self.buy(self.coinMarketType,str(Qty2*okcoin_sell_1_price),exchange="okcoin",sell_1_price=okcoin_sell_1_price)
 
             elif okcoin_buy_1_price > huobi_sell_1_price:  # 获利信号：OKcoin卖，huobi买
                 self.timeLog("发现信号")
@@ -366,16 +358,16 @@ class BanzhuanStrategy(object):
                 Qty = min(Qty, accountInfo[helper.coinTypeStructure[self.coinMarketType]["okcoin"]["coin_str"]], helper.myRound(accountInfo[helper.coinTypeStructure[self.coinMarketType]["huobi"]["market_str"]] / huobi_sell_1_price), 4)
                 Qty = helper.getRoundedQuantity(Qty, self.coinMarketType)
 
-                if Qty < self.huobi_min or Qty < self.okcoin_min:
-                    self.timeLog("数量:%f 小于交易所最小交易数量(火币最小数量:%f, okcoin最小数量:%f),因此无法下单并忽略该信号"%(Qty, self.huobi_min, self.okcoin_min))
+                if Qty < self.huobi_min_quantity or Qty < self.okcoin_min_quantity:
+                    self.timeLog("数量:%f 小于交易所最小交易数量(火币最小数量:%f, okcoin最小数量:%f),因此无法下单并忽略该信号"%(Qty, self.huobi_min_quantity, self.okcoin_min_quantity))
                     continue
                 else:
                     # step1: 先处理卖
                     executed_qty = self.sell(self.coinMarketType,str(Qty),exchange="okcoin")
-
-                    # step2: 再执行买
-                    Qty2 = min(executed_qty, Qty)
-                    self.buy(self.coinMarketType,str(Qty2*huobi_sell_1_price),exchange="huobi")
+                    if executed_qty is not None:
+                        # step2: 再执行买
+                        Qty2 = min(executed_qty, Qty)
+                        self.buy(self.coinMarketType,str(Qty2*huobi_sell_1_price),exchange="huobi")
 
 
 if __name__ == "__main__":
