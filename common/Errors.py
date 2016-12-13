e1b14954fe2301a9b624193dbe06ad869fa3dd2a
lanjianzhong <lanjason@foxmail.com>
2016-12-13 13:50:06 +0800

    project refactor

--- common/Errors.py
+++ common/Errors.py
@@ -10,20 +10,22 @@
 class StartRunningTimeEmptyError(ValueError):
     pass
 
+
 class InvalidFilterError(ValueError):
     pass
+
 
 class TypeError(ValueError):
     pass
 
+
 class EmptySecurityError(ValueError):
     pass
+
 
 class InvalidSecurityError(ValueError):
     pass
 
+
 class InvalidFrequencyError(ValueError):
     pass
-
-
-
